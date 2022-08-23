154a652017e878eff2b5ca4366be679ed1877986
Lachlan <21005784@massey.ac.nz>
2022-08-24 02:59:49 +1200

    First Commit

---
+++ functions.js
@@ -0,0 +1,36 @@
+var limit = 1000;
+
+$(document).ready(mobileCheck);
+$(window).resize(mobileCheck);
+
+
+
+function mobileCheck() {
+    	var resW = $(window).width();
+    	if (resW < limit) {
+		document.getElementsByTagName('ul')[0].style.display = "none";
+		$('#LogoBox').css("float", "none"); 
+		$('#SideBar').css("width", "50px");
+		$('.dropdown').css("display", "inline-block");
+
+		$('li').css("float", "none"); 
+		$('li a').css("font-size", "30px");
+		$('li').css("display", "block");
+
+		   
+
+    	}
+	else {
+		$('.dropdown').css("display", "none");
+
+		$('ul').css("display", "block");
+  		$('#LogoBox').css("float", "left");
+		$('#SideBar').css("width", "300px");   
+
+
+		$('li').css("float", "left"); 
+		$('li a').css("font-size", "2.9vw");
+		$('li').css("display", "inline");
+		
+	}
+}
