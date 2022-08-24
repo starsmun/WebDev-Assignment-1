76f999efe3b7d824c99b3d883ae5e559e3069d2c
Lachlan <21005784@massey.ac.nz>
2022-08-24 19:36:33 +1200

    Fixed Issue with content area

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-var limit = 1000;
+var limit = 1100;
 
 $(document).ready(mobileCheck);
 $(window).resize(mobileCheck);
@@ -9,9 +9,11 @@ function mobileCheck() {
     	var resW = $(window).width();
     	if (resW < limit) {
 		document.getElementsByTagName('ul')[0].style.display = "none";
-		$('#LogoBox').css("float", "none"); 
+		$('#LogoBox').css("float", "none");
+		$('#ContentArea').css("width", "calc(100% - 50px)");
 		$('#SideBar').css("width", "50px");
 		$('.dropdown').css("display", "inline-block");
+		
 
 		$('li').css("float", "none"); 
 		$('li a').css("font-size", "30px");
@@ -26,7 +28,7 @@ function mobileCheck() {
 		$('ul').css("display", "block");
   		$('#LogoBox').css("float", "left");
 		$('#SideBar').css("width", "300px");   
-
+		$('#ContentArea').css("width", "calc(100% - 300px)");
 
 		$('li').css("float", "left"); 
 		$('li a').css("font-size", "2.9vw");
